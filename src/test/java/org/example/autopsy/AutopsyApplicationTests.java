3d7b1080d558076c9c18280f9e8ac9d93599398c
Triết Nguyễn <trietnguyen@To-ri-e-3.local>
2026-07-29 21:24:14 -0700

    first commit

---
+++ src/test/java/org/example/autopsy/AutopsyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.autopsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutopsyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
